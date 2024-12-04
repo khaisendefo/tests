e00f0646cfcc56c30b19a81e983629ec5bab5399
khaisendefo <khaisendefo@gmail.com>
2024-12-05 00:42:09 +0300

    First commit

---
+++ inc/highslide/highslide.config.js
@@ -0,0 +1,13 @@
+hs.graphicsDir = docRoot + 'inc/highslide/graphics/';
+hs.showCredits = false;
+hs.creditsPosition = 'bottom left';
+hs.outlineType = 'custom';
+hs.dimmingOpacity = 0.2;
+hs.dimmingDuration = 0;
+hs.easing = 'easeInBack';
+hs.easingClose = 'easeOutBack';
+hs.captionEval = 'this.a.title';
+hs.headingEval = 'this.thumb.title';
+hs.onDimmerClick = function() {
+   return false;
+}
